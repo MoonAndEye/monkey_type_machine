e44f7e0c0b994b242085e8ad94533893db38f33c
Moonandeye <MoonAndEye@users.noreply.github.com>
2016-07-08 11:23:45 +0900

    把 pandas 模組拿掉了, 改成 csv

--- save_load.py
+++ save_load.py
@@ -4,28 +4,33 @@ Created on Fri Jun 17 16:58:21 2016
 
 把之前跑到一半的資料拿出來
 """
-import os
-import pandas as pd
+
 import random as rd
 import time
+import csv
 
 
 """
 下面這一段,是先讀取前面的結果,打出字數,剩餘字數,使用時間
 """
-
-b4 = pd.read_csv("history.txt", encoding="utf-8")
-
-count = len(b4.ix[:])
-
-load_last = b4.loc[count-1]
 load = []
-for each in load_last:
-    load.append(each)
-"""
-load 這個 list 存放的，就是上一次的打出字數,剩餘字數,所花時間 
+#b4 = pd.read_csv("history.txt", encoding="utf-8")
+with open("history.txt", "r") as f:
+    precsv = csv.reader(f)
+    csvf = list(precsv)
+    load_last = sum(1 for row in csvf)
+    count = load_last - 1
+    for each in csvf[count]:
+        each = float(each)
+        load.append(each)
+    print(load)
 
-下面是做出讀檔功能
+
+
+"""
+#load 這個 list 存放的，就是上一次的打出字數,剩餘字數,所花時間 
+
+#下面是做出讀檔功能
 """
 
 with open("remain.txt", "r", encoding="utf-8") as load_f:
@@ -36,7 +41,7 @@ with open("remain.txt", "r", encoding="utf-8") as load_f:
 last = temp.split(",")
 
 """
-last 裡面的list 就是上次還剩下的字,還沒被猴子打出來
+#last 裡面的list 就是上次還剩下的字,還沒被猴子打出來
 """
 #print(last[0])
 #print(last[0][1:-1])
@@ -73,4 +78,3 @@ while len(last) > 0:
         with open("remain.txt", "w", encoding = "utf-8") as rf:
             rf.write(str(last))
     flag = flag + 1
-
